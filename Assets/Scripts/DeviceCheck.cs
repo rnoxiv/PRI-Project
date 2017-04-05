c853bc027e20530411842f1e46b2539d9a47edee
rnoxiv <sabatier.david.93@gmail.com>
2017-04-05 10:51:23 +0200

    First commit

---
+++ Assets/Scripts/DeviceCheck.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeviceCheck : MonoBehaviour {
+
+	// Use this for initialization
+
+    private bool checkedSyst = false;
+    public GameObject touchManager;
+
+	void Start () {
+
+        if (!checkedSyst)
+        {
+            Debug.Log(SystemInfo.deviceType);
+            if (SystemInfo.deviceType == DeviceType.Desktop)
+            {
+                (touchManager.GetComponent("StandardInput") as MonoBehaviour).enabled = false;
+
+            }else
+            {
+                (touchManager.GetComponent("TuioInput") as MonoBehaviour).enabled = false;
+                print("Tablet");
+            }
+        }
+    }
+}
